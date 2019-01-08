fb6615e632b98b7ddc3343b99ad3159707b8ccd8
srnpr <srnpr@srnprpro13.lan>
2019-01-08 23:14:25 +0800

    change image

--- compile/expand/expand-elm/react_uimage.js
+++ compile/expand/expand-elm/react_uimage.js
@@ -4,7 +4,8 @@ var MexpandReactUicon = /** @class */ (function () {
     function MexpandReactUicon() {
     }
     MexpandReactUicon.prototype.expandOpen = function (oItem) {
-        item_support_1.ItemSupportReact.checkPropFull(oItem, "src", "source", "require(", ")", "'");
+        item_support_1.ItemSupportReact.checkPropFull(oItem, "src", "source", "{require(", ")}", "'");
+        item_support_1.ItemSupportReact.checkPropFull(oItem, "url", "source", "{{uri:", "}}", "'");
         item_support_1.ItemSupportReact.checkPropWithQuotes(oItem, "mode", "resizeMode");
         return oItem;
     };
--- src/expand/expand-elm/react_uimage.ts
+++ src/expand/expand-elm/react_uimage.ts
@@ -10,7 +10,8 @@ class MexpandReactUicon implements CTF.ItransformExpandItem {
 
     expandOpen(oItem: CTF.ItransformItemInfo) {
 
-        processItem.checkPropFull(oItem,"src","source","require(",")","'");
+        processItem.checkPropFull(oItem,"src","source","{require(",")}","'");
+        processItem.checkPropFull(oItem,"url","source","{{uri:","}}","'");
         processItem.checkPropWithQuotes(oItem,"mode","resizeMode");
         return oItem;
     }
